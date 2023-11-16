3a93d0950db42b4e0ef49a776440f9d4c3d701d9
Justin Headley <headley.justin@gmail.com>
2023-11-15 21:39:20 -0800

    refactor: cleanup

--- autogen_simple_group_test.py
+++ autogen_simple_group_test.py
@@ -1,10 +1,11 @@
 import autogen
-from autogen import GroupChat, GroupChatManager
 
 from better_group_chat import BetterGroupChat, BetterGroupChatManager
 from src.agent_prompts import (
     PYTHON_EXPERT_AGENT_SYSTEM_PROMPT,
     FUNCTION_CALLING_AGENT_SYSTEM_PROMPT,
+    USER_PROXY_SYSTEM_PROMPT,
+    AGENT_AWARENESS_SYSTEM_PROMPT,
 )
 
 from dotenv import load_dotenv
@@ -12,7 +13,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import os
-import json
 import copy
 
 from src.utils.agent_utils import get_end_intent
@@ -32,7 +32,6 @@ config_list4 = [
 ]
 
 llm_config4 = {
-    # "request_timeout": 600,
     "seed": 42,
     "config_list": config_list4,
     "temperature": 0.1,
@@ -40,84 +39,32 @@ llm_config4 = {
 
 WORK_DIR = "NEW_TEST_DIR"
 
-
 user_proxy = autogen.UserProxyAgent(
     name="UserProxy",
-    system_message="""You are a proxy for the user. You will be able to see the conversation between the assistants. You will ONLY be prompted when there is a need for human input or the conversation is over. If you are ever prompted directly for a resopnse, always respond with: 'Thank you for the help! I will now end the conversation so the user can respond.'
-    
-!!!IMPORTANT: NEVER respond with anything other than the above message. If you do, the user will not be able to respond to the assistants.""",
+    system_message=USER_PROXY_SYSTEM_PROMPT,
     human_input_mode="TERMINATE",
     is_termination_msg=lambda x: get_end_intent(x) == "end",
-    # code_execution_config={"last_n_messages": 3, "work_dir": 'TEST_DIR'},
     code_execution_config=False,
     llm_config=llm_config4,
 )
 
-# assistant1 = autogen.AssistantAgent(
-#     name="Jokester",
-#     system_message="You are an expert at writing jokes and NOTHING ELSE. Secifically you DO NOT write code.",
-#     llm_config=llm_config4,
-#     # is_termination_msg=lambda x: get_end_intent(x) == "end",
-# )
-
-# assistant2 = autogen.AssistantAgent(
-#     name="Critiquer",
-#     system_message="You are an expert at critiquing jokes and NOTHING ELSE. Secifically you DO NOT write code.",
-#     llm_config=llm_config4,
-#     # is_termination_msg=lambda x: get_end_intent(x) == "end",
-# )
-
-assistant1 = autogen.AssistantAgent(
+code_reviewer = autogen.AssistantAgent(
     name="CodeReviewer",
     system_message="""You are an expert at reviewing code and suggesting improvements. Pay particluar attention to any potential syntax errors. Also, remind the Coding agent that they should always provide FULL and COMPLILABLE code and not shorten code blocks with comments such as '# Other class and method definitions remain unchanged...' or '# ... (previous code remains unchanged)'.""",
     llm_config=llm_config4,
-    # is_termination_msg=lambda x: get_end_intent(x) == "end",
 )
 
-assistant2 = autogen.AssistantAgent(
+agent_awareness_expert = autogen.AssistantAgent(
     name="AgentAwarenessExpert",
-    system_message="""You are an expert at understanding the nature of the agents in the team. Your job is to help guide agents in their task, making sure that suggested actions align with your knowledge. Specifically, you know that:
-    - AGENTS: Agents are Large Language Models (LLMs). The most important thing to understand about Large Language Models (LLMs) to get the most leverage out of them is their latent space and associative nature. LLMs embed knowledge, abilities, and concepts ranging from reasoning to planning, and even theory of mind. This collection of abilities and content is referred to as the latent space. Activating the latent space of an LLM requires the correct series of words as inputs, creating a useful internal state of the neural network. This process is similar to how the right cues can prime a human mind to think in a certain way. By understanding and utilizing this associative nature and latent space, you can effectively leverage LLMs for various applications​​.
-    - CODE EXECUTION: Some agents have the ability to suggest/write code, while others have the ability to execute code. A single agent may or may not have both of these abilities.
-    - READING FILES: Agents cannot "read" (i.e know the contents of) a file unless the file contents are printed to the console and added to the agent conversation history. When analyzing/evaluating code (or any other file), it is IMPORTANT to actually print the content of the file to the console and add it to the agent conversation history. Otherwise, the agent will not be able to access the file contents. ALWAYS first check if a function is available to the team to read a file (such as "read_file") as this will automatically print the contents of the file to the console and add it to the agent conversation history.
-    - CONTEXT KNOWLEDGE: Context knowledge is not accessible to agents unless it is explicitly added to the agent conversation history, UNLESS the agent specifically has functionality to access outside context.
-    - AGENT COUNCIL: The agents in a team are guided by an "Agent Council" that is responsible for deciding which agent should act next. The council may also give input into what action the agent should take.
-    - FUNCTION CALLING: Some agents have specific functions registered to them. Each registered function has a name, description, and arguments. Agents have been trained to detect when it is appropriate to "call" one of their registered functions. When an agents "calls" a function, they will respond with a JSON object containing the function name and its arguments. Once this message has been sent, the Agent Council will detect which agent has the capability of executing this function. The agent that executes the function may or may not be the same agent that called the function.
-    """,
+    system_message=AGENT_AWARENESS_SYSTEM_PROMPT,
     llm_config=llm_config4,
-    # is_termination_msg=lambda x: get_end_intent(x) == "end",
 )
 
-
-# assistant3 = autogen.AssistantAgent(
-#     name="Coder",
-#     # Clone llm_config4 and update it with the functions
-#     llm_config = coder_llm_config,
-#     # llm_config=llm_config4.update({"functions": functions}),
-#     # llm_config=llm_config4,
-#     system_message=DEFAULT_CODING_AGENT_SYSTEM_MESSAGE.format(work_dir=work_dir),
-#     # code_execution_config={"last_n_messages": 5, "work_dir": 'TEST_DIR'},
-#     # is_termination_msg=lambda x: get_end_intent(x) == "end",
-#     function_map={"read_file": read_file},
-# )
-
-assistant3 = autogen.AssistantAgent(
+python_expert = autogen.AssistantAgent(
     name="PythonExpert",
     llm_config=llm_config4,
     system_message=PYTHON_EXPERT_AGENT_SYSTEM_PROMPT,
-    # code_execution_config={"last_n_messages": 5, "work_dir": 'TEST_DIR'},
-    # is_termination_msg=lambda x: get_end_intent(x) == "end",
-    # function_map={"read_file": read_file},
 )
-
-# assistant4 = autogen.AssistantAgent(
-#     name="Executor",
-#     system_message="You are an expert at executing code and NOTHING ELSE. Secifically you DO NOT write code. If any other agents provide a code block for you to execute, you must execute it and provide the output.",
-#     llm_config=llm_config4,
-#     code_execution_config={"last_n_messages": 5, "work_dir": work_dir},
-#     # is_termination_msg=lambda x: get_end_intent(x) == "end",
-# )
-
 
 functions = [
     {
@@ -206,12 +153,10 @@ def execute_code_block(lang, code_block):
 function_llm_config = copy.deepcopy(llm_config4)
 function_llm_config["functions"] = functions
 
-assistant4 = autogen.AssistantAgent(
+function_calling_expert = autogen.AssistantAgent(
     name="FunctionCallingExpert",
     system_message=FUNCTION_CALLING_AGENT_SYSTEM_PROMPT,
     llm_config=function_llm_config,
-    # code_execution_config={"last_n_messages": 5, "work_dir": work_dir},
-    # is_termination_msg=lambda x: get_end_intent(x) == "end",
     function_map={
         "read_file": read_file,
         "save_file": save_file,
@@ -220,7 +165,13 @@ assistant4 = autogen.AssistantAgent(
 )
 
 groupchat = BetterGroupChat(
-    agents=[user_proxy, assistant1, assistant2, assistant3, assistant4],
+    agents=[
+        user_proxy,
+        code_reviewer,
+        agent_awareness_expert,
+        python_expert,
+        function_calling_expert,
+    ],
     messages=[],
     max_round=100,
     persona_discussion=True,
@@ -228,15 +179,11 @@ groupchat = BetterGroupChat(
 )
 manager = BetterGroupChatManager(groupchat=groupchat, llm_config=llm_config4)
 
-# user_proxy.initiate_chat(manager, message="Please come up with a list of 10 excellent jokes for a standup comedy routine. They must appeal to an adult AND child audience. When you are satisfied that the jokes are sufficient, please provide a python script that will output one of the jokes at random when it is run.")
-
 user_proxy.initiate_chat(
     manager,
     message="""I have a python file that I need cleaning up. The file is located at ./better_group_chat.py. Please clean up the file apply best code practices and good commenting without changing the functionality. I would also like to improve some logs to look better with some color and formatting. Lets use the "colored" python package to do this. Lets keep/refactor the following logs: CHAT_MANAGER_RESPONSE, GET_NEXT_ACTOR_RESPONSE (but only print the "analysis" property), and NEXT_SPEAKER. When you're done please save the file under "agent_better_group_chat.py" for me to review. """,
 )
 
-# TODO: Add ability for the AGENT_COUNCIL to decide if a new agent should be created and added to the groupchat.
+# TODO: Add a function that allows injection of a new agent into the group chat.
 
-# TODO: Add function to directly "save" files
-# TODO: get a better understanding of how agents currently execute and save files
-# TODO: Think of ways to give agents a better understanding of their capabilities and limitations
+# TODO: Add a function that allows spawning a new group chat with a new set of agents.
--- src/agent_prompts.py
+++ src/agent_prompts.py
@@ -19,6 +19,19 @@ IMPORTANT: While you can read files on the file system, you can ONLY write to th
 
 """
 
+USER_PROXY_SYSTEM_PROMPT = """You are a proxy for the user. You will be able to see the conversation between the assistants. You will ONLY be prompted when there is a need for human input or the conversation is over. If you are ever prompted directly for a resopnse, always respond with: 'Thank you for the help! I will now end the conversation so the user can respond.'
+    
+!!!IMPORTANT: NEVER respond with anything other than the above message. If you do, the user will not be able to respond to the assistants."""
+
+AGENT_AWARENESS_SYSTEM_PROMPT = """You are an expert at understanding the nature of the agents in the team. Your job is to help guide agents in their task, making sure that suggested actions align with your knowledge. Specifically, you know that:
+    - AGENTS: Agents are Large Language Models (LLMs). The most important thing to understand about Large Language Models (LLMs) to get the most leverage out of them is their latent space and associative nature. LLMs embed knowledge, abilities, and concepts ranging from reasoning to planning, and even theory of mind. This collection of abilities and content is referred to as the latent space. Activating the latent space of an LLM requires the correct series of words as inputs, creating a useful internal state of the neural network. This process is similar to how the right cues can prime a human mind to think in a certain way. By understanding and utilizing this associative nature and latent space, you can effectively leverage LLMs for various applications​​.
+    - CODE EXECUTION: Some agents have the ability to suggest/write code, while others have the ability to execute code. A single agent may or may not have both of these abilities.
+    - READING FILES: Agents cannot "read" (i.e know the contents of) a file unless the file contents are printed to the console and added to the agent conversation history. When analyzing/evaluating code (or any other file), it is IMPORTANT to actually print the content of the file to the console and add it to the agent conversation history. Otherwise, the agent will not be able to access the file contents. ALWAYS first check if a function is available to the team to read a file (such as "read_file") as this will automatically print the contents of the file to the console and add it to the agent conversation history.
+    - CONTEXT KNOWLEDGE: Context knowledge is not accessible to agents unless it is explicitly added to the agent conversation history, UNLESS the agent specifically has functionality to access outside context.
+    - AGENT COUNCIL: The agents in a team are guided by an "Agent Council" that is responsible for deciding which agent should act next. The council may also give input into what action the agent should take.
+    - FUNCTION CALLING: Some agents have specific functions registered to them. Each registered function has a name, description, and arguments. Agents have been trained to detect when it is appropriate to "call" one of their registered functions. When an agents "calls" a function, they will respond with a JSON object containing the function name and its arguments. Once this message has been sent, the Agent Council will detect which agent has the capability of executing this function. The agent that executes the function may or may not be the same agent that called the function.
+    """
+
 FUNCTION_CALLING_AGENT_SYSTEM_PROMPT = """You are an expert at calling functions. You do not write code, you only call functions that have been registered to you.
 
 """
