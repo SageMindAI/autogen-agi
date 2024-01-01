00737e085c0be6b7988647d72057e915715daff0
Justin Headley <headley.justin@gmail.com>
2024-01-01 12:27:29 -0800

    Add support for multiple models in OAI_CONFIG_LIST.json.example

--- autogen_test.py
+++ autogen_test.py
@@ -11,12 +11,31 @@ load_dotenv()
 
 # Load LLM configuration from environment or a file
 config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST.json")
-llm_config = {"config_list": config_list}
+
+config_list_mistral = [config for config in config_list if config["model"] == "ollama/mistral"]
+
+config_list_deepseek_coder = [config for config in config_list if config["model"] == "ollama/deepseek-coder:6.7b"]
+
+
+if len(config_list_mistral) > 0:
+    llm_config_user_proxy = {"config_list": config_list_mistral}
+else:
+    llm_config_user_proxy = {"config_list": config_list}
+
+if len(config_list_deepseek_coder) > 0:
+    llm_config_assistant = {"config_list": config_list_deepseek_coder}
+else:
+    llm_config_assistant = {"config_list": config_list}
+
+
+print("llm_config_user_proxy:", llm_config_user_proxy)
+print("llm_config_assistant:", llm_config_assistant)
+
 
 # Create an AssistantAgent instance
 assistant = AssistantAgent(
     name="assistant",
-    llm_config=llm_config,
+    llm_config=llm_config_assistant,
 )
 
 # Create a UserProxyAgent instance with autonomous settings
@@ -25,7 +44,7 @@ user_proxy = UserProxyAgent(
     human_input_mode="NEVER",  # No human input will be solicited
     max_consecutive_auto_reply=10,  # Maximum number of consecutive auto-replies
     code_execution_config={"work_dir": "working"},  # Working directory for code execution
-    llm_config=llm_config,  # LLM configuration for generating replies
+    llm_config=llm_config_user_proxy,  # LLM configuration for generating replies
 )
 
 # Initiate a conversation with a task description
